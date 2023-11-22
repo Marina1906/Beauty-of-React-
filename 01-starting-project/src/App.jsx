6e1168ba34a2a0b02e157300207e5b5f7377c305
Marina Makević <128428504+Marina1906@users.noreply.github.com>
2023-11-23 00:13:07 +0100

    edited code

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,67 +1,70 @@
 import reactImg from './assets/react-core-concepts.png';
+import {CORE_CONCEPTS} from './data.js';
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 function genRandomInt (max) {
-  return Math.floor (Math.random()* (max + 1));
+  return Math.floor (Math.random () * (max + 1));
 }
 
-
-function Header() {
+function Header () {
+  const description = reactDescriptions[genRandomInt (2)];
   return (
     <header>
-    <img src={reactImg} alt="Stylized atom" />
-    <h1>React Essentials</h1>
-    <p>
-      {reactDescriptions[genRandomInt(2)]} React concepts you will need for almost any app you are
-      going to build!
-    </p>
-  </header>
-  );
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
 
+        {description}React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
 }
 
-
-function App() {
+function CoreConcept({img, title, description}) {
   return (
-    <div>  
-      <Header/>
-  <main>
-        <h2>Time to get started!</h2>
+    <li>
+      <img src={img} alt={props.titile} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
+
+function App () {
+  return (
+    <div>
+      <Header />
+      <main>
+
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              img={CORE_CONCEPTS[0].img}
+            />
+            <CoreConcept
+              title={CORE_CONCEPTS[1].title}
+              description={CORE_CONCEPTS[1].description}
+              img={CORE_CONCEPTS[1].img}
+            />
+            <CoreConcept
+              title={CORE_CONCEPTS[2].title}
+              description={CORE_CONCEPTS[2].description}
+              img={CORE_CONCEPTS[2].img}
+            />
+            <CoreConcept
+              title={CORE_CONCEPTS[3].title}
+              description={CORE_CONCEPTS[3].description}
+              img={CORE_CONCEPTS[3].img}
+            />
+          </ul>
+        </section>
       </main>
     </div>
   );
 }
 
 export default App;
-
-import componentsImg from './assets/components.png';
-import propsImg from './assets/config.png';
-import jsxImg from './assets/jsx-ui.png';
-import stateImg from './assets/state-mgmt.png';
-
-export const CORE_CONCEPTS = [
-  {
-    image: componentsImg,
-    title: 'Components',
-    description:
-      'The core UI building block - compose the user interface by combining multiple components.',
-  },
-  {
-    image: jsxImg,
-    title: 'JSX',
-    description:
-      'Return (potentially dynamic) HTML(ish) code to define the actual markup that will be rendered.',
-  },
-  {
-    image: propsImg,
-    title: 'Props',
-    description:
-      'Make components configurable (and therefore reusable) by passing input data to them.',
-  },
-  {
-    image: stateImg,
-    title: 'State',
-    description:
-      'React-managed data which, when changed, causes the component to re-render & the UI to update.',
-  },
-];
