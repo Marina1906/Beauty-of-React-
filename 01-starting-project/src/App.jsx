c11899ede06649294464e8a4cea7ed321320da78
Marina Makević <128428504+Marina1906@users.noreply.github.com>
2023-11-26 21:46:52 +0100

    Update App.jsx

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -4,13 +4,13 @@ import {CORE_CONCEPTS} from '../../data.js';
 import Header from './Components/Header/Header.jsx';
 import CoreConcept from './Components/CoreConcepts.jsx';
 import TabButton from './Components/Tab.Button.jsx';
-import { EXAMPLES } from './data.js';
+import {EXAMPLES} from './data.js';
 
 function App () {
-  const [selectedTopic, setSelectedTopic] = useState('components');
+  const [selectedTopic, setSelectedTopic] = useState ();
 
   function handleSelect (selectedButton) {
-    setSelectedTopic(selectedButton);
+    setSelectedTopic (selectedButton);
     console.log (selectedTopic);
   }
 
@@ -56,12 +56,17 @@ function App () {
             <TabButton onSelect={() => handleSelect ('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect ('state')}>State</TabButton>
           </menu>
-          <div id="tab-content"></div>
-          <h3>{EXAMPLES[selectedTopic].title }</h3>
-          <p>{EXAMPLES[selectedTopic].description } </p>
-          <pre>
-            <code>{EXAMPLES[selectedTopic].code } </code>
-          </pre>
+
+          {!selectedTopic ? <p>Please select a topic.</p> : null}
+          {selectedTopic
+            ? <div id="tab-content">
+                <h3>{EXAMPLES[selectedTopic].title}</h3>
+                <p>{EXAMPLES[selectedTopic].description} </p>
+                <pre>
+                  <code>{EXAMPLES[selectedTopic].code} </code>
+                </pre>
+              </div>
+            : null}
         </section>
       </main>
     </div>
